cdd59db7a013cf4c042559967167fea43731fde0
Bandss <foreignbandss@hotmail.com>
2025-11-22 11:52:48 -0500

    Create hehe.c

---
+++ hehe.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // Includes the standard input/output library
+
+int main() { // The main function, where program execution begins
+    printf("Hello, World!\n"); // Prints "Hello, World!" to the console
+    return 0; // Indicates successful program execution
+}
