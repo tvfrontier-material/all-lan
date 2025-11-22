a88cda92a9750dc64d1194e7f7fa6d0b1f5f67f6
Bandss <foreignbandss@hotmail.com>
2025-11-22 11:49:01 -0500

    Update index.vb

--- index.vb
+++ index.vb
@@ -2,6 +2,3 @@
 Console.Write("Please enter your name: ")
 Dim name = Console.ReadLine()
 Dim currentDate = DateTime.Now
-Console.WriteLine($"Hello, {name}, on {currentDate:d} at {currentDate:t}")
-Console.Write("Press any key to continue...")
-Console.ReadKey(True)
